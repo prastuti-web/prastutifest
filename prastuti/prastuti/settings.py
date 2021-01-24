9b058e305f9f505c6581dcf2724f1b6b8bc4c7a4
Aman <amansingh.eee18@itbhu.ac.in>
2021-01-24 11:17:58 +0530

    remove secret key form settings.py

--- prastuti/prastuti/settings.py
+++ prastuti/prastuti/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's0+e9-bacqoj1237)@_u8=*i=ajmk^ffk801o48$q1=spi&8vo'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
